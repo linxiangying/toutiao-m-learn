7ac5beed4f974d3f8c177d878a1de9dc223a1c8a
linxiangying <450758905@qq.com>
2022-05-16 16:00:49 +0800

    引入vant组件库

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 // 引入css
 import '@/style/index.less'
+// 引入vant组件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
